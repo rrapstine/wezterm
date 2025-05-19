34fabb366a32ef4c466e337cd57d342fdf834be7
Richard Rapstine <richard.rapstine@gmail.com>
2025-05-18 20:38:38 -0500

    Switch from bar to tabline

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Pull in plugins
-local bar = wezterm.plugin.require 'https://github.com/adriankarlen/bar.wezterm'
+-- local bar = wezterm.plugin.require 'https://github.com/adriankarlen/bar.wezterm'
+-- local bar_config = require 'plugins/bar'
 
 -- Create a variable for the multiplexer layer
 local mux = wezterm.mux
@@ -28,9 +29,21 @@ end)
 
 -- Window configuration
 config.window_background_opacity = 0.8
-config.window_decorations = 'RESIZE'
-config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 32
+config.window_decorations = 'RESIZE'
+config.window_padding = config.window_padding or {}
+config.window_padding.left = 4
+config.window_padding.right = 4
+config.window_padding.top = 4
+config.window_padding.bottom = 4
+
+config.colors = config.colors or {}
+config.colors.tab_bar = config.colors.tab_bar or {}
+-- config.colors.tab_bar.background = require('tabline.config').theme.normal_mode.c.bg
+config.status_update_interval = 500
 
 -- Font settings
 config.font = wezterm.font 'Hack Nerd Font'
@@ -94,7 +107,8 @@ config.keys = {
 }
 
 -- Initialize plugins
-bar.apply_to_config(config)
+-- bar.apply_to_config(config, bar_config)
+require 'plugins/tabline'
 
 -- Return the configuration to wezterm
 return config
